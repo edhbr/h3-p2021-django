a88776f5c9dc80332a0657ec0e2cd27c7abee8c0
lihbr <lihbr@users.noreply.github.com>
2019-05-27 12:23:29 +0200

    :wrench: sh > bat

--- quickInstall.bat
+++ quickInstall.bat
@@ -1,23 +1,17 @@
-virtualenv . -p python
-
-echo "Windows (win) or Linux/Mac (lin): "
-read os
-if [ $os -eq "win"];
-then
-"Scripts/activate.bat"
-else
-source bin/activate
-fi
+virtualenv . -p python && "Scripts/activate.bat"
+echo "Virtualenv installed"
 pip install -r requirements.txt
+echo "PIP dependencies installed"
 mv settings.sample.ini settings.ini
 mv db.sample.sqlite3 db.sqlite3
 python manage.py collectstatic --no-input
+echo ""
 echo "Install done"
 echo ""
 echo "Edit settings.ini with your values"
 echo "$ vim settings.ini"
 echo ""
 echo "Run server with :"
-read "$ python manage.py runserver"
+echo "$ python manage.py runserver"
 echo ""
-read "Press enter to quit"
+pause
